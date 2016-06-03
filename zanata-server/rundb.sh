d641b4e73af045ef9f75d3b1f96283d438ebdd20
Sean Flanigan <sflaniga@redhat.com>
2016-06-03 18:13:59 +1000

    Use utf8 (not utf8mb)

--- zanata-server/rundb.sh
+++ zanata-server/rundb.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 docker run --name zanatadb \
   -e MYSQL_USER=zanata -e MYSQL_PASSWORD=password -e MYSQL_DATABASE=zanata -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
-  -d mariadb:10.1
+  -d mariadb:10.1 \
+  --character-set-server=utf8 --collation-server=utf8_general_ci
+
 echo 'Please use the command "docker logs zanatadb" to check that MariaDB starts correctly.'
