0abde7ee73d8d304a5b9ad2558dc42eecd4a3663
Sean Flanigan <sflaniga@redhat.com>
2016-06-03 18:13:59 +1000

    Use MySQL 5.7, utf8mb, large prefix, DYNAMIC row format

--- zanata-server/rundb.sh
+++ zanata-server/rundb.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 docker run --name zanatadb \
   -e MYSQL_USER=zanata -e MYSQL_PASSWORD=password -e MYSQL_DATABASE=zanata -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
-  -d mariadb:10.1
-echo 'Please use the command "docker logs zanatadb" to check that MariaDB starts correctly.'
+  -d mysql:5.7 \
+  --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+echo 'Please use the command "docker logs zanatadb" to check that MySQL starts correctly.'
