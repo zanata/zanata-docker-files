4dd303dd1c2f7bba0cd27dd4826f7f221931851f
Ding-Yi Chen <dchen@redhat.com>
2016-07-22 12:35:09 +1000

    Use extend regex instead

--- zanata-server/runapp.sh
+++ zanata-server/runapp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 ScriptDir=$(dirname $(readlink -f $0))
-ZanataVer=$(sed -n -e '/^ARG\s*ZANATA_VERSION/ s/^ARG\s*ZANATA_VERSION\s*=\s*\(.*\)\s*/\1/p' < $ScriptDir/Dockerfile)
+ZanataVer=$(sed -r -n -e '/^ARG\s+ZANATA_VERSION/ s/^ARG\s+ZANATA_VERSION\s*=\s*(.*)\s*/\1/p' < $ScriptDir/Dockerfile)
 docker run --rm --name zanata --link zanatadb:db -p 8080:8080 -it zanata/server:$ZanataVer
