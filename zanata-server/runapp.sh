367d415e15f20ca5ce651d7206ec835516059e9d
Ding-Yi Chen <dingyichen@gmail.com>
2016-07-22 14:54:57 +1000

    upate(Dockerfile): Revise Dockerfile and config (#7)
    * update(Dockerfile): Revise Dockerfile and scripts

--- zanata-server/runapp.sh
+++ zanata-server/runapp.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-docker run --name zanata --link zanatadb:db -p 8080:8080 -it zanata/server:3.9.0
+#!/bin/bash -eu
+ScriptDir=$(dirname $(readlink -f $0))
+ZanataVer=$(sed -r -n -e '/^ARG\s+ZANATA_VERSION/ s/^ARG\s+ZANATA_VERSION\s*=\s*(.*)\s*/\1/p' < $ScriptDir/Dockerfile)
+docker run --rm --name zanata --link zanatadb:db -p 8080:8080 -it zanata/server:$ZanataVer
