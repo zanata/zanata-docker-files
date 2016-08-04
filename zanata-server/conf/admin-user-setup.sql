e9d1abdcf2128290edc6f8b0f0ac6a15ec43fe68
Koji Iimura <koji.iimura@o-03022-win.dena.local>
2016-08-04 12:09:26 +0900

    change from 4 to 5 	modified:   zanata-server/conf/admin-user-setup.sql

--- zanata-server/conf/admin-user-setup.sql
+++ zanata-server/conf/admin-user-setup.sql
@@ -6,4 +6,4 @@ VALUES (1, now(), now(), 1, 1, 'UZMf4PIqtTBGAo9wWKuTpg==', 'admin');
 INSERT INTO HPerson (id, creationDate, lastChanged, versionNum, email, `name`, accountId)
 VALUES (1, now(), now(), 1, 'admin@noreply.org', 'Administrator', 1);
 
-INSERT INTO HAccountMembership(accountId, memberOf) VALUES (1, 4);
+INSERT INTO HAccountMembership(accountId, memberOf) VALUES (1, 5);
