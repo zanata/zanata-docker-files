6bc446a332de91f33b30736ec6761cbb7ad5fa1e
Ding-Yi Chen <dingyichen@gmail.com>
2016-11-07 13:53:50 +1000

    doc(ZNTA-1435): Update Docker image document (#10)
    * doc(ZNTA-1435): Update Docker image document
     https://zanata.atlassian.net/browse/ZNTA-1435
    > This depends on https://zanata.atlassian.net/browse/ZNTA-1380

--- zanata-server/rundb.sh
+++ zanata-server/rundb.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -eu
-### This program setups the data volume and database containers for Zanata
+### This program sets up the data volume and database containers for Zanata
 
 : ${ZANATA_MYSQL_USER:=zanata}
 : ${ZANATA_MYSQL_PASSWORD:=password}
 : ${ZANATA_MYSQL_DATABASE:=zanata}
 
-## Create missing data volume
+## Create zanata data volume if it is missing
 if ! (docker volume ls -q | grep zanata-data) ; then
     docker volume create --name zanata-data
 fi
 
-## Run container when no container named zanatadb
 if [ -z $(docker ps -aq -f name=zanatadb) ];then
+    ## Create new container if zanatadb does not exist
     docker run --name zanatadb \
 	-e MYSQL_USER=${ZANATA_MYSQL_USER} -e MYSQL_PASSWORD=${ZANATA_MYSQL_PASSWORD} \
 	-e MYSQL_DATABASE=${ZANATA_MYSQL_DATABASE} -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
@@ -19,6 +19,7 @@ if [ -z $(docker ps -aq -f name=zanatadb) ];then
         -d mariadb:10.1 \
 	--character-set-server=utf8 --collation-server=utf8_general_ci
 elif [ -n $(docker ps -aq -f name=zanatadb -f status=exited) ];then
+    ## Start container if zanatadb stopped
     docker start zanatadb
 fi
 
