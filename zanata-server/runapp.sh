7ff8a935778321ca8557d718cd6d54a5aa1b94d4
Carlos Munoz <chavo16@hotmail.com>
2016-12-15 16:13:49 +1000

    refactor: multiple changes to Dockerfile and executables (#12)
    Add ZANATA_HOME environment variable instead of user.home in
    standalone.xml
    Use curl to download dependencies in the Dockerfile instead of
    needing to install yum
    Remove some dependencies between the two running containers
    Change some volume mappings
    Add message specifying Zanata version being run to runapp.sh

--- zanata-server/runapp.sh
+++ zanata-server/runapp.sh
@@ -5,12 +5,19 @@ if [ -n "${ZANATA_VERSION-}" ];then
     ## Use the version from set environment ZANATA_VERSION
     ZanataVer=$ZANATA_VERSION
 else
-    ## Use the ZANATA_VERSION from Dockerfile, line "ARG ZANATA_VERSION="
-    ZanataVer=$(sed -r -n -e '/^ARG\s+ZANATA_VERSION/ s/^ARG\s+ZANATA_VERSION\s*=\s*(.*)\s*/\1/p' < $ScriptDir/Dockerfile)
+    ## Use the latest version (not necessarily the version defined in the Dockerfile)
+    ZanataVer=latest
+fi
+
+echo "Preparing container for Zanata ($ZanataVer)"
+
+## Create zanata-files volume if it is missing
+if ! (docker volume ls -q | grep zanata-files) ; then
+    docker volume create --name zanata-files
 fi
 
 : ${ZANATA_PORT:=8080}
-DockerOptArray=(--name zanata --volumes-from zanatadb -v zanatadb:/opt/jboss --link zanatadb:db -p $ZANATA_PORT:8080)
+DockerOptArray=(--name zanata -v zanata-files:/var/lib/zanata -e ZANATA_HOME=/var/lib/zanata --link zanatadb:db -p $ZANATA_PORT:8080)
 
 ## If specified ZANATA_DAEMON_MODE=1, it runs Zanata as daemon,
 ## Otherwise it will remove the container after program exit.
